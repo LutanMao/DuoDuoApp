5717c8e9b98778f7911f51eae67a834853955f9d
maozhendong <zhendong.mao@smartautotech.com>
2016-12-20 11:56:41 +0800

    add crop iamge

---
+++ app/src/main/java/com/mao/duoduo/activity/PersonalActivity.java
@@ -0,0 +1,22 @@
+package com.mao.duoduo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mao on 16-12-20.
+ */
+public class PersonalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+
+}
---
+++ cropimage/src/main/java/com/mao/cropimage/BuildConfig.java
@@ -0,0 +1,16 @@
+package com.mao.cropimage;
+
+/**
+ * Created by Mao on 16-12-20.
+ */
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+    public static final String APPLICATION_ID = "com.kevin.crop";
+    public static final String BUILD_TYPE = "release";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 3;
+    public static final String VERSION_NAME = "1.0.2";
+
+    public BuildConfig() {
+    }
+}
