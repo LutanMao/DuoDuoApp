644b8f97d7a54f6e4ecf2ae554b2e0990e751c4e
maozhendong <zhendong.mao@smartautotech.com>
2016-12-21 18:22:08 +0800

    add configuration class

---
+++ app/src/main/java/com/mao/duoduo/AppConfiguration.java
@@ -0,0 +1,11 @@
+package com.mao.duoduo;
+
+/**
+ * Created by Mao on 16-12-21.
+ */
+public class AppConfiguration {
+
+    // 是否发布版本
+    public static boolean isDebug = true;
+
+}
--- app/src/main/java/com/mao/duoduo/utils/MaoLog.java
+++ app/src/main/java/com/mao/duoduo/utils/MaoLog.java
@@ -1,34 +1,33 @@
 package com.mao.duoduo.utils;
 
 import android.util.Log;
+import com.mao.duoduo.AppConfiguration;
 
 /**
  * Created by Mao on 2016/12/19.
  */
 public class MaoLog {
 
-    private static boolean isDebug = true;
-
     public static void i(String tag, String msg) {
-        if (isDebug) {
+        if (AppConfiguration.isDebug) {
             Log.i(tag, msg);
         }
     }
 
     public static void d(String tag, String msg) {
-        if (isDebug) {
+        if (AppConfiguration.isDebug) {
             Log.d(tag, msg);
         }
     }
 
     public static void e(String tag, String msg) {
-        if (isDebug) {
+        if (AppConfiguration.isDebug) {
             Log.e(tag, msg);
         }
     }
 
     public static void w(String tag, String msg) {
-        if (isDebug) {
+        if (AppConfiguration.isDebug) {
             Log.w(tag, msg);
         }
     }
