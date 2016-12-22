092583a65a712b61bb4a8f99ca11defe4d535d55
maozhendong <zhendong.mao@smartautotech.com>
2016-12-22 18:28:37 +0800

    back press handle

--- app/src/main/java/com/mao/duoduo/activity/BaseActivity.java
+++ app/src/main/java/com/mao/duoduo/activity/BaseActivity.java
@@ -3,6 +3,7 @@ package com.mao.duoduo.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 import com.mao.duoduo.MaoApplication;
 
 /**
@@ -10,14 +11,12 @@ import com.mao.duoduo.MaoApplication;
  */
 public abstract class BaseActivity extends AppCompatActivity {
 
-    private static final int MAX_CLICK_DELAY_TIME = 500;
+    private static final int MAX_CLICK_DELAY_TIME = 1000;
 
     // 当前时间
     private long mCurrentTime;
     // 上一次点击时间
     private long mLastTime;
-    // 点击Back次数
-    private int mClickTime = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,8 +29,10 @@ public abstract class BaseActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         mCurrentTime = System.currentTimeMillis();
-        mClickTime ++;
-        if (mCurrentTime - mLastTime < MAX_CLICK_DELAY_TIME) {
+        if (mCurrentTime - mLastTime > MAX_CLICK_DELAY_TIME) {
+            mLastTime = mCurrentTime;
+            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+        } else {
             MaoApplication.getInstance().mActivityStack.AppExit();
         }
     }
