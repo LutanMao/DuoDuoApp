ed190e7fc5f83876dd16e61c1503392191ccfe0b
maozhendong <zhendong.mao@smartautotech.com>
2016-12-23 15:13:42 +0800

    user butterknife

--- app/src/main/java/com/mao/duoduo/activity/MainActivity.java
+++ app/src/main/java/com/mao/duoduo/activity/MainActivity.java
@@ -3,13 +3,13 @@ package com.mao.duoduo.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.mao.duoduo.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.btn_register)
     Button mBtnRegister;
@@ -20,32 +20,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
-        initListener();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_register:
-                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(registerIntent);
-                break;
-            case R.id.btn_login:
-                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(loginIntent);
-                break;
-        }
+    @OnClick(R.id.btn_login)
+    public void login() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    @OnClick(R.id.btn_register)
+    public void register() {
+        Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 
     private void initView() {
         setContentView(R.layout.activity_main);
         // 必须要setContentView之后bind
         ButterKnife.bind(this);
-    }
-
-    private void initListener() {
-        mBtnRegister.setOnClickListener(this);
-        mBtnLogin.setOnClickListener(this);
     }
 
 }
