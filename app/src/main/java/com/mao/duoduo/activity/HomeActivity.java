0a24c615d4243e3ab4eb29cc59a6ef1adbcc8d6f
maozhendong <zhendong.mao@smartautotech.com>
2016-12-26 13:31:05 +0800

    add Baidu location function

--- app/src/main/java/com/mao/duoduo/activity/HomeActivity.java
+++ app/src/main/java/com/mao/duoduo/activity/HomeActivity.java
@@ -11,13 +11,15 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.*;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnCheckedChanged;
-import butterknife.OnPageChange;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import butterknife.*;
 import cn.bmob.v3.BmobUser;
+import com.baidu.location.*;
 import com.bumptech.glide.Glide;
+import com.mao.duoduo.MaoApplication;
 import com.mao.duoduo.R;
 import com.mao.duoduo.adapter.HomePagerAdapter;
 import com.mao.duoduo.bean.User;
@@ -48,10 +50,6 @@ public class HomeActivity extends BaseActivity implements IHomeView {
     DrawerLayout mDrawerLayout;
     @BindView(R.id.scroll_view)
     PullToZoomScrollViewEx mScrollView;
-    
-    private ListView mListView;
-    private CircleImageView mCirHeader;
-
     @BindView(R.id.vp_content)
     ViewPager mViewPager;
     @BindView(R.id.rg_bottom)
@@ -65,39 +63,33 @@ public class HomeActivity extends BaseActivity implements IHomeView {
     @BindView(R.id.rb_text4)
     RadioButton mRbText4;
 
+    private CircleImageView mCirHeader;
+
+    private Unbinder mUnBinder;
+
     private HomePagerAdapter mHomePagerAdapter;
     private ActionBarDrawerToggle mDrawerToggle;
-    private ArrayAdapter mArrayAdapter;
     private List<Fragment> mFragmentList;
 
-
-    private String[] lvs = {"List Item 01", "List Item 02", "List Item 03", "List Item 04"};
+    private LocationClient mLocationClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mHomePresenter = new HomePresenter(this);
+        initLocation();
         initView();
         initData();
     }
 
-    /**
-     * ViewPager滑动过程监听
-     */
-//    @OnPageChange(value = R.id.vp_content, callback = OnPageChange.Callback.PAGE_SCROLL_STATE_CHANGED)
-//    public void onPageChanged() {
-//        MaoLog.e(TAG, "PAGE_SCROLL_STATE_CHANGED");
-//    }
-//
-//    @OnPageChange(value = R.id.vp_content, callback = OnPageChange.Callback.PAGE_SELECTED)
-//    public void onPageSelected() {
-//        MaoLog.e(TAG, "PAGE_SELECTED");
-//    }
-//
-//    @OnPageChange(value = R.id.vp_content, callback = OnPageChange.Callback.PAGE_SCROLLED)
-//    public void onPageScrolled() {
-//        MaoLog.e(TAG, "PAGE_SCROLLED");
-//    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mUnBinder.unbind();
+        mLocationClient.unRegisterLocationListener(mLocationListener);
+        mLocationClient = null;
+        mLocationListener = null;
+    }
 
     @OnPageChange(R.id.vp_content)
     public void onPageSelected(int position) {
@@ -149,9 +141,7 @@ public class HomeActivity extends BaseActivity implements IHomeView {
     @Override
     protected void initView() {
         setContentView(R.layout.activity_home);
-
-        ButterKnife.bind(this);
-
+        mUnBinder = ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
         mToolbar.setTitle("ToolBar");
         mToolbar.setTitleTextColor(Color.parseColor("#ffffff"));
@@ -184,7 +174,6 @@ public class HomeActivity extends BaseActivity implements IHomeView {
         View zoomView = LayoutInflater.from(this).inflate(R.layout.profile_zoom_view, null, false);
         View contentView = LayoutInflater.from(this).inflate(R.layout.profile_content_view, null, false);
 
-        mListView = ButterKnife.findById(contentView, R.id.lv_content);
         mCirHeader = ButterKnife.findById(headView, R.id.civ_header);
 
         mCirHeader.setOnClickListener(new View.OnClickListener() {
@@ -194,9 +183,6 @@ public class HomeActivity extends BaseActivity implements IHomeView {
                 startActivity(intent);
             }
         });
-
-        mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lvs);
-        mListView.setAdapter(mArrayAdapter);
 
         Glide.with(this).load(BmobUser.getCurrentUser(User.class).getAvatar())
                 .into((ImageView) headView.findViewById(R.id.civ_header));
@@ -208,6 +194,37 @@ public class HomeActivity extends BaseActivity implements IHomeView {
         mScrollView.setScrollContentView(contentView);
     }
 
+    private void initLocation() {
+        mLocationClient = new LocationClient(MaoApplication.getInstance());
+        LocationClientOption option = new LocationClientOption();
+        //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，默认gcj02，设置返回的定位结果坐标系
+        option.setCoorType("bd09ll");
+        int span=1000;
+        //可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setScanSpan(span);
+        //可选，设置是否需要地址信息，默认不需要
+        option.setIsNeedAddress(true);
+        //可选，默认false,设置是否使用gps
+        option.setOpenGps(true);
+        //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setLocationNotify(true);
+        //可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationDescribe(true);
+        //可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIsNeedLocationPoiList(true);
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.setIgnoreKillProcess(false);
+        //可选，默认false，设置是否收集CRASH信息，默认收集
+        option.SetIgnoreCacheException(false);
+        //可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        option.setEnableSimulateGps(false);
+        mLocationClient.setLocOption(option);
+        mLocationClient.registerLocationListener(mLocationListener);
+        mLocationClient.start();
+    }
+
     private void initData() {
         mFragmentList = new ArrayList<Fragment>();
         mFragmentList.add(new Text1Fragment());
@@ -217,5 +234,73 @@ public class HomeActivity extends BaseActivity implements IHomeView {
         mHomePagerAdapter.setFragments(mFragmentList);
         mHomePagerAdapter.notifyDataSetChanged();
     }
+
+    private BDLocationListener mLocationListener = new BDLocationListener() {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            sb.append("\ncity : ");
+            sb.append(location.getCity());
+            if (location.getLocType() == BDLocation.TypeGpsLocation){// GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());// 单位：公里每小时
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());// 单位：米
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());// 单位度
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){// 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            } else {
+                mLocationClient.stop();
+            }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            List<Poi> list = location.getPoiList();// POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+            Toast.makeText(MaoApplication.getInstance(), sb.toString(), Toast.LENGTH_SHORT).show();
+        }
+    };
 
 }
