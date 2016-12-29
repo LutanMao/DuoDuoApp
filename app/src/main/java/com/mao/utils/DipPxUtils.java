96d9c60dadf7260e99a240b770f74c60d2655583
maozhendong <zhendong.mao@smartautotech.com>
2016-12-29 11:51:09 +0800

    remove the uitls package

---
+++ app/src/main/java/com/mao/utils/DipPxUtils.java
@@ -0,0 +1,18 @@
+package com.example.administrator.sharebutton;
+
+/**
+ * Created by wiiliam on 16-11-3.
+ */
+public class DipPxUtils {
+
+    public static int dip2px(int dipValue) {
+        final float scale = MaoApplication.getInstance().getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    public static int px2dip(int pxValue) {
+        final float scale = MaoApplication.getInstance().getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+}
